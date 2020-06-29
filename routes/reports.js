3e09886dc5f44b17828ede1609be32df7135a7f2
Alejandro <aesbetancourt@outlook.com>
2020-06-29 18:29:31 -0400

    reports routes

--- routes/reports.js
+++ routes/reports.js
@@ -13,6 +13,23 @@ router.post('/data', (req, res) => {
     });
 })
 
+
+// Ruta para listar request
+router.get('/get_request', async (req, res) => {
+    const request = await pool.query('SELECT * FROM request INNER JOIN client ON request.cli_id = client.cli_id where req_status <> 5 ORDER BY client.cli_name ASC, req_title ASC ');
+    console.log(request);
+    res.json(request);
+});
+
+// Ruta para listar users
+
+router.get('/get_user', async (req, res) => {
+    const users = await pool.query('SELECT * FROM user ORDER BY usr_name ASC');
+    console.log(users);
+    res.json(users);
+});
+
+
 //Funcion para imprimir errores
 function printError(e){
 	if(e!=null){
