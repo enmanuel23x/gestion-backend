6b9511474c1d26f3d15dd42edcbbae8ce3c7e89e
ramon <rvielma94@gmail.com>
2020-06-30 17:56:41 -0400

    Agregado vista por defecto index.html

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 //NPM Requires
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 //Project's own requires
 const config  = require('./config/config').server;
 //Initializations
 const app = express();
-const report = require('./routes/reports')
-const booking = require('./routes/booking')
+const report = require('./routes/reports');
+const booking = require('./routes/booking');
 //Express Settings
 app.use(cors());
 //Express Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 //Express Routes
-app.use('/report', report)
-app.use('/booking',booking)
+// Vista por defecto para la ruta raiz
+app.get("/", function (request, response) {
+    response.sendFile(path.join(__dirname, 'views/index.html'));
+});
+app.use('/report', report);
+app.use('/booking',booking);
 //Start Server
 const PORT = process.env.PORT || config.port;
 
--- routes/reports.js
+++ routes/reports.js
@@ -3,15 +3,15 @@ const express = require('express');
 //Project's own requires
 const pool = require('../database');
 //Initializations 
-const router = express.Router()
+const router = express.Router();
 //Rutas
 router.post('/data', (req, res) => {
-    const { email } = req.body
+    const { email } = req.body;
     pool.query('SELECT * FROM example WHERE email = "'+email+'"', (err, conn) => {
-        printError(err)
+        printError(err);
         res.json(conn)
     });
-})
+});
 
 
 // Ruta para listar request
@@ -31,15 +31,22 @@ router.get('/get_user', async (req, res) => {
 });
 
 router.get('/get_req/:id', async (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
     const result = await pool.query('SELECT boo_start_date, boo_end_date, boo_percentage, (SELECT usr_name FROM user WHERE usr_id = db_gestion_ocupacion.booking.usr_id) AS name, (SELECT req_title FROM request WHERE req_id = db_gestion_ocupacion.booking.req_id) AS req_name, (SELECT cli_id FROM request WHERE req_id = db_gestion_ocupacion.booking.req_id) AS cli_id FROM db_gestion_ocupacion.booking WHERE req_id ='+ id);
+    res.json(result);
+});
+
+
+router.get('/get_cli/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await pool.query('SELECT cli_id FROM request WHERE req_id = '+ id);
     res.json(result);
 });
 
 //Funcion para imprimir errores
 function printError(e){
-	if(e!=null){
-		console.log(e)
-	}
+    if(e!=null){
+        console.log(e)
+    }
 }
-module.exports = router
+module.exports = router;
