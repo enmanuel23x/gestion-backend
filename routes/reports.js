e33c6d82e74a4df75e600244d399ac6d14c80130
Alejandro <aesbetancourt@outlook.com>
2020-08-10 12:53:51 -0400

    search activities by client

--- routes/reports.js
+++ routes/reports.js
@@ -3,7 +3,7 @@ const express = require('express');
 const DateDiff = require('date-diff');
 //Project's own requires
 const pool = require('../database');
-//Initializations 
+//Initializations
 const router = express.Router()
 //Rutas
 
@@ -15,8 +15,22 @@ router.get('/get_request', async (req, res) => {
     res.json(request);
 });
 
-// Ruta para listar users
+// Ruta para obtener los clientes
+router.get('/get_client', async (req, res) => {
+    const request = await pool.query(`SELECT * FROM client`);
+    //console.log(request);	    console.log(request);
+    res.json(request);
+});
+// Ruta para listar requests en base a un cli_id
+router.get('/get_reqbycli/:id', async (req, res) => {
+    const {id} = req.params
+    const ESTADO = 'open';
+  const request = await pool.query(`SELECT * FROM request where sta_id = '${ESTADO}' AND cli_id = '${id}' ORDER BY req_title ASC`);
+    //console.log(request);	    console.log(request);
+    res.json(request);
+});
 
+// Ruta para listar users
 router.get('/get_user', async (req, res) => {
     const users = await pool.query('SELECT * FROM user ORDER BY usr_name ASC');
     //console.log(users);
@@ -25,9 +39,9 @@ router.get('/get_user', async (req, res) => {
 // Ruta para request con su desviacion
 router.get('/get_req_desv', async (req, res) => {
 
-    const result = await pool.query(`SELECT req_id, cli_id, req_title, req_responsable, req_date, req_init_date, req_final_date, req_real_final_date, req_deviations_ptge, req_day_desv, 
-                                     (SELECT cli_name FROM client WHERE cli_id = dbgestionocupacion.request.cli_id) AS cli_name 
-                                     FROM dbgestionocupacion.request 
+    const result = await pool.query(`SELECT req_id, cli_id, req_title, req_responsable, req_date, req_init_date, req_final_date, req_real_final_date, req_deviations_ptge, req_day_desv,
+                                     (SELECT cli_name FROM client WHERE cli_id = dbgestionocupacion.request.cli_id) AS cli_name
+                                     FROM dbgestionocupacion.request
                                      ORDER BY cli_name ASC, req_title ASC`);
     res.json(result);
 });
@@ -36,11 +50,11 @@ router.get('/get_req_desv', async (req, res) => {
 // Ruta para activities con su desviacion
 router.get('/get_act_desv/:id', async (req, res) => {
     const { id } = req.params
-    const result = await pool.query(`SELECT act_id, activities.req_id,  act_trello_name, act_init_date, act_init_real_date, act_end_date, act_real_end_date, act_desv_percentage, act_day_desv, 
-    (SELECT cli_name FROM client WHERE cli_id IN (SELECT cli_id FROM request WHERE request.req_id = '${id}')) AS cli_name, 
-    (SELECT req_title FROM request WHERE request.req_id = '${id}') AS req_title, 
-    (SELECT req_responsable  FROM request WHERE request.req_id = '${id}') AS req_responsable  
-    FROM activities WHERE activities.req_id = '${id}' 
+    const result = await pool.query(`SELECT act_id, activities.req_id,  act_trello_name, act_init_date, act_init_real_date, act_end_date, act_real_end_date, act_desv_percentage, act_day_desv,
+    (SELECT cli_name FROM client WHERE cli_id IN (SELECT cli_id FROM request WHERE request.req_id = '${id}')) AS cli_name,
+    (SELECT req_title FROM request WHERE request.req_id = '${id}') AS req_title,
+    (SELECT req_responsable  FROM request WHERE request.req_id = '${id}') AS req_responsable
+    FROM activities WHERE activities.req_id = '${id}'
     ORDER BY act_init_date DESC , act_end_date DESC`);
     res.json(result);
 });
